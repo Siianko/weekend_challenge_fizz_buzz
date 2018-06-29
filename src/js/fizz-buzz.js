25e0e09bf17c62a7a2390efa135e0b3ba36d1d0c
Siianko <tsiyanko@gmail.com>
2018-06-29 10:47:36 +0200

    added logics for the fizz-buzkata and added specs to test

--- src/js/fizz-buzz.js
+++ src/js/fizz-buzz.js
@@ -1,10 +1,14 @@
 function FizzBuzz() {
+   const hasZeroRemainder = (number, divider) => {
+       return (number % divider) === 0
+   }
+   
     this.check = (number) => {
-        if(number % 15 === 0) {
+        if(hasZeroRemainder(number, 15)) {
             return 'fizz-buzz';
-        } else if(number % 5 === 0) {
+        } else if(hasZeroRemainder(number, 5)) {
         return 'buzz';
-        } else if(number % 3 === 0) { 
+        } else if(hasZeroRemainder(number, 3)) { 
             return 'fizz';
         } else { 
         return number;
